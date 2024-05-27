e89aed77c07af3381115fef6916d90c8f9a0fc84
tomaschalde <chalde_tomas@hotmail.es>
2024-05-27 19:52:12 -0300

    Implementaciones en Pets, Search, Shelters y Users
    - Implementacion de endpoints
    
    - Implementacion de servicios.
    
    - Implementación de repositorios (falta la logica)

--- back/src/shelters/shelters.service.ts
+++ back/src/shelters/shelters.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { ShelterRepository } from './shelters.repository';
+import { ShelterEntity } from 'src/entidades/shelter.entity';
 
 @Injectable()
-export class SheltersService {}
+export class SheltersService {
+    constructor(private readonly sheltersRepository : ShelterRepository){}
+
+    getShelters(){
+        return this.sheltersRepository.getShelters();
+    }
+
+    getShelterById(id : string){
+        return this.sheltersRepository.getShelterById(id);
+    }
+
+    addShelter(shelter : ShelterEntity){
+        return this.sheltersRepository.addShelter(shelter);
+    }
+
+    deleteShelter(id : string) {
+        return this.sheltersRepository.deleteShelter(id);
+    }
+}
