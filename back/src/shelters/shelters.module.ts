6da18a6d3efe0ae38dced58817a953d223dca8db
MDiaz8778 <68826380+mauro8778@users.noreply.github.com>
2024-05-27 02:01:23 -0300

    asdasdad

--- back/src/shelters/shelters.module.ts
+++ back/src/shelters/shelters.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { SheltersService } from './shelters.service';
 import { SheltersController } from './shelters.controller';
 import { ShelterRepository } from './shelters.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ShelterEntity } from './shelter.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([ShelterEntity])],
   providers: [SheltersService],
   controllers: [SheltersController],
   exports:[ShelterRepository]
--- back/src/users/users.module.ts
+++ back/src/users/users.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { UserRepository } from './User.Repository';
+import { UserEntity } from './user.Entity';
+import { VolunteerEntity } from './volunteers.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([UserEntity,VolunteerEntity])],
   controllers: [UserController],
   providers: [UserService],
   exports:[UserRepository]
