5b77f08d7cb42dee34bfd1c51c3dc930549b861a
Jongabee <104025249+Jongabee@users.noreply.github.com>
2024-05-28 00:35:07 -0400

    Agrego carpeta components interface y utils

---
+++ front/src/interface/IRefugios.ts
@@ -0,0 +1,8 @@
+
+
+export interface IRefugios {
+    id: number;
+    name: string;
+    provincia: string;
+    zona: string
+}
---
+++ front/src/utils/refugios.ts
@@ -0,0 +1,43 @@
+import { IRefugios } from "../interface/IRefugios"
+
+const Refugios : IRefugios[] = [
+    {
+        id: 1,
+        name: "4 Patas",
+        provincia: "Buenos Aires",
+        zona: "Zona Oeste",
+    },
+    {
+        id: 2,
+        name: "Refugio Canino",
+        provincia: "Buenos Aires",
+        zona: "Zona Norte",
+    },
+    {
+        id: 3,
+        name: "Hogar de Perros Felices",
+        provincia: "Buenos Aires",
+        zona: "Zona Sur",
+    },
+    {
+        id: 4,
+        name: "Refugio de Animales Amistad",
+        provincia: "Buenos Aires",
+        zona: "CABA",
+    },
+    {
+        id: 5,
+        name: "Refugio Felino Paz",
+        provincia: "Buenos Aires",
+        zona: "Zona Sur",
+    },
+    {
+        id: 6,
+        name: "Hogar de Gatos Felices",
+        provincia: "Buenos Aires",
+        zona: "Zona Oeste",
+    },
+]
+
+
+export default Refugios
