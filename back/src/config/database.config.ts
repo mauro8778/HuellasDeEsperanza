0e78a3a857e96707033e03896031d9c3ec4fb5e3
MaxiSalguero <salgueromaximiliano8@gmail.com>
2024-05-30 18:58:43 -0300

    Implementacion de Auth User

--- back/src/config/database.config.ts
+++ back/src/config/database.config.ts
@@ -2,7 +2,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME } from './envs';
 
-
 const dbConfig: DataSourceOptions = {
   type: 'postgres',
   database: DB_NAME,
--- back/src/entidades/user.Entity.ts
+++ back/src/entidades/user.Entity.ts
@@ -1,72 +1,43 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import {v4 as uuid} from "uuid"
-import { DonationEntity } from "./donation.entity";
-import { AdoptionEntity } from "./adoption.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { DonationEntity } from './donation.entity';
+import { AdoptionEntity } from './adoption.entity';
 
-
-@Entity
-({
-    name: 'users'
+@Entity({
+  name: 'users',
 })
-export class UserEntity{
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string = uuid();
 
-    @PrimaryGeneratedColumn("uuid")
-    id: string = uuid()
+  @Column({
+    type: 'varchar',
+  })
+  name: string;
 
+  @Column({
+    type: 'varchar',
+  })
+  last_name: string;
 
-    @Column({
-        type:"varchar",
-        nullable: false,
-    })
-    name: string
+  @Column()
+  birthdate: Date;
 
+  @Column({
+    type: 'bigint',
+    nullable: true,
+  })
+  phone?: number | undefined;
 
-    @Column({
-        type:"varchar",
-        nullable: false,
-    })
-    last_name: string
+  @Column({
+    type: 'varchar',
+    nullable: true,
+  })
+  location?: string | undefined;
 
+  @OneToMany(() => DonationEntity, (donation) => donation.user)
+  donations: DonationEntity[];
 
-    @Column({
-        type:"varchar",
-        unique: true,
-        nullable: false,
-    })
-    email: string
-    
-
-    @Column({
-        type:"varchar",
-        nullable: false,
-    })
-    password: string
-
-
-    @Column({
-        nullable: false,
-    })
-    birthday: Date
-
-
-    @Column({
-        type: "int",
-        nullable: true
-    })
-    phone?: number | undefined
-
-
-    @Column({
-        type: "varchar",
-        nullable: true
-    })
-    location?: string | undefined
-
-    @OneToMany(() => DonationEntity, donation => donation.user)
-    donations: DonationEntity[];
-
-
-    @OneToMany( ()=> AdoptionEntity, adoptions => adoptions.user)
-    adoptions: AdoptionEntity[]
-
+  @OneToMany(() => AdoptionEntity, (adoptions) => adoptions.user)
+  adoptions: AdoptionEntity[];
 }
