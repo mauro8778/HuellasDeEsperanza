8db6c6a95276d7606b7538c278f6e8603a3b7982
juank132 <juancarlos_castillo@hotmail.com.ar>
2024-06-04 05:53:52 -0300

    Preload de 10 Shelters y 10 Pets

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SerchModule } from './serch/serch.module';
@@ -15,27 +15,40 @@ import { Auth0Module } from './auth0/auth0.module';
 import { MailModule } from './mails/mail.module';
 import { ConfigModule } from '@nestjs/config';
 import { MercadoPagoModule } from './mercado-pago/mercado-pago.module';
+import { PreloadService } from './app.PreloadService';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ShelterEntity } from './entidades/shelter.entity';
+import { PetsEntity } from './entidades/pets.entity';
 
 @Module({
   imports: [
       ConfigModule.forRoot({
         isGlobal: true,
       }),
-    databaseConfig,
-    SerchModule,
-    UsersModule,
-    SheltersModule,
-    DonationModule,
-    AdoptionModule,
-    AuthModule,
-    PetsModule,
-    ChatModule,
-    FileUploadModule,
+      databaseConfig,
+      SerchModule,
+      UsersModule,
+      SheltersModule,
+      DonationModule,
+      AdoptionModule,
+      AuthModule,
+      PetsModule,
+      ChatModule,
+      FileUploadModule,
     Auth0Module,
     MailModule,
-    MercadoPagoModule
+    MercadoPagoModule,
+    TypeOrmModule.forFeature([ShelterEntity, PetsEntity]),
   ],
   controllers: [AppController], 
-  providers: [AppService],
+  providers: [AppService, PreloadService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(private preloadService: PreloadService){}
+
+  async onModuleInit() {
+    await this.preloadService.loadPets();
+
+    await this.preloadService.loadShelters();
+  }
+}
