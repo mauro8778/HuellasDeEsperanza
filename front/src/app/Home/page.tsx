493a73b3174482d5932541f26a493ff298f25945
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2024-06-04 17:39:05 -0300

    painas lentas solucionadas , navar solucionado , home borrado

--- front/src/app/Home/page.tsx
+++ front/src/app/Home/page.tsx
@@ -1,34 +1,9 @@
-import React from "react";
-import Image from "next/image";
-import RandomAnimalCards from "@/components/Card-Animals/RandomAnimalsCards";
-import Mascotas from "@/utils/mascotas";
-import Refugios from "@/utils/refugios";
-import RandomRefugiosCards from "@/components/Refugios/RandomRefugiosCards";
+import React from 'react'
 
-export const Home: React.FC = () => {
+const Home = () => {
   return (
-    <div className="relative w-full min-h-screen overflow-hidden">
-      {/* Banner */}
-      <div className="relative w-full h-96">
-        <Image
-          src="/imgs/bannerNav.png"
-          alt="Wave Background"
-          layout="fill"
-          objectFit="cover"
-          objectPosition="center"
-        />
-      </div>
-      
-      <div className="relative z-0 mt-10 ml-10">
-        <RandomAnimalCards mascotas={Mascotas}/>
-      </div>
+    <div className='text-3xl flex justify-center'>Home</div>
+  )
+}
 
-      <div className="relative z-0 mt-10 ml-10">
-        <RandomRefugiosCards refugios={Refugios}/>
-      </div>
-      
-    </div>
-  );
-};
-
-export default Home;
+export default Home
