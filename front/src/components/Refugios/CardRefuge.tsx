f0ffa6ed757b64efee9dc4e1f7c8e4c97f50c571
Jongabee <104025249+Jongabee@users.noreply.github.com>
2024-06-07 20:34:12 -0400

    estilos de las cards arregladas

--- front/src/components/Refugios/CardRefuge.tsx
+++ front/src/components/Refugios/CardRefuge.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import Image from 'next/image';
 import { IRefugios } from '@/interface/IRefugios';
@@ -9,21 +8,19 @@ const truncateDescription = (text: string, maxLength: number) => {
 };
 
 const CardRefuge: React.FC<{ refugio: IRefugios }> = ({ refugio }) => {
-  const truncatedDescription = truncateDescription(refugio.description, 100);
+  const truncatedTitle = refugio.shelter_name ? truncateDescription(refugio.shelter_name, 25) : '';
+  const truncatedDescription = refugio.description ? truncateDescription(refugio.description, 60) : '';
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-4">
-      <div style={{ height: '200px', overflow: 'hidden', position: 'relative' }}>
-        <Image src={refugio.imgUrl} alt={refugio.name} layout="fill" objectFit="cover" className="rounded-t-lg" />
+    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-4 p-5">
+      <div className="h-64 relative overflow-hidden rounded-t-lg">
+        <Image src={refugio.imgUrl} alt={refugio.name} layout="fill" objectFit="cover" />
       </div>
-      <div className="p-5">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{refugio.name}</h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{refugio.location} - {refugio.zona}</p>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{truncatedDescription}</p>
-        <a href={`/refugios/${refugio.id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-pink-600 rounded-lg hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      <div className="mt-4">
+        <h5 className="text-xl font-semibold text-gray-900 dark:text-white">{truncatedTitle}</h5>
+        <p className="mt-2 text-gray-700 dark:text-gray-400">{refugio.location} - {refugio.zona}</p>
+        <p className="mt-2 text-gray-700 dark:text-gray-400">{truncatedDescription}</p>
+        <a href={`/refugios/${refugio.id}`} className="inline-block mt-4 px-4 py-2 text-sm font-semibold text-white bg-pink-600 rounded-lg hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2">
           Leer más
-          <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-          </svg>
         </a>
       </div>
     </div>
