a6f5f459ffba5d057d1b48d60e2808fd488c14e2
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2024-06-11 01:04:46 -0300

    reparando mi fornt

---
+++ front/src/app/checkout/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Checkout from '../checkout'
+
+const CheckoutMercadoPago = () => {
+  return (
+    <div>
+        <Checkout />
+    </div>
+  )
+}
+
+export default CheckoutMercadoPago
