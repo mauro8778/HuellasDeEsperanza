fc46c858bb6831bde6859e870779fcf6c9c39894
MDiaz8778 <maurodiaz8778@gmail.com>
2024-06-12 03:47:31 -0300

    asdaad

--- front/src/components/RefugioDetail/RefugioDetail.tsx
+++ front/src/components/RefugioDetail/RefugioDetail.tsx
@@ -1,15 +1,14 @@
-'use client';
-import { useEffect, useState } from 'react';
-import Swal from 'sweetalert2';
+"use client"
 import { IRefugios } from '@/interface/IRefugios';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { FaPaw } from 'react-icons/fa'; 
 import FavoriteRefuge from './FavoriteRefuge';
+import { useState } from 'react';
+import Swal from 'sweetalert2';
 
 export const RefugioDetail: React.FC<IRefugios> = ({ id, name, description, imgUrl, location, zona, shelter_name, phone }) => {
-  const [selectedAmount, setSelectedAmount] = useState<number | null>(null);
-  const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [selectedAmount, setSelectedAmount] = useState<number | null>(null); 
   const router = useRouter();
   const [isFavorite, setIsFavorite] = useState(false);
 
@@ -26,58 +25,34 @@ export const RefugioDetail: React.FC<IRefugios> = ({ id, name, description, imgU
       Swal.fire('Por favor, selecciona un monto para donar');
       return;
     }
-
-    if (!accessToken) {
-      Swal.fire('Por favor, inicia sesión para donar');
-      return;
-    }
-
-    
+  
+    // saco la info del local storage
     const donations = localStorage.getItem('donations');
     const parsedDonations = donations ? JSON.parse(donations) : [];
-    console.log("Donaciones ya guardadas:", parsedDonations);
-
-    const newDonation = {
-      shelter: { id, name: shelter_name },
-      amount: selectedAmount
-    };
-    console.log('Nuevas donaciones:', newDonation);
-
-    const updatedDonations = [...parsedDonations, newDonation];
-
-    
-    localStorage.setItem('donations', JSON.stringify(updatedDonations));
-
-    console.log('Donaciones actualizadas:', updatedDonations);
-
-    
-    const donationData = updatedDonations.map((donation: any) => ({
+  
+    // Aca se crea el objeto para enviar la donacion al back
+    const donationData = parsedDonations.map((donation: any) => ({
       id: donation.shelter.id,
       price: Number(donation.amount)
     }));
-
-    console.log('Se creó el objeto donationData:', donationData);
-
+  
     try {
-      
+      // Hacer la solicitud POST
       const response = await fetch("https://huellasdesperanza.onrender.com/carrito", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${accessToken}`
+          "Content-Type": "application/json"
         },
         body: JSON.stringify(donationData)
       });
-
-      console.log('Respuesta del backend:', response);
-
+  
       if (!response.ok) {
         throw new Error("Error al agregar donación al carrito");
       }
-
+  
       Swal.fire(`Tu donación de $${selectedAmount} fue agregada con éxito`);
       window.location.href = '/refugios';
-    } catch (error: any) {
+    } catch (error) {
       console.error('Error al agregar donación al carrito:', error);
       Swal.fire({
         title: "¡Error!",
@@ -93,31 +68,22 @@ export const RefugioDetail: React.FC<IRefugios> = ({ id, name, description, imgU
       });
     }
   };
+  
 
   return (
     <>
       <div className="max-w-3xl mx-auto mt-5 mb-5 bg-white shadow-xl rounded-lg overflow-hidden">
-
-        <div className="flex flex-col md:flex-row">
-          <div className="md:w-1/2">
-            <Image
-              src={imgUrl ?? ''}
-              alt={name ?? ''}
-              width={500}
-              height={300}
-              className="object-cover h-full w-full"
-            />
-          </div>
-          <div className="md:w-1/2 p-5 flex flex-col items-center justify-center text-center">
-            <h1 className='antialiased font-bold text-xl mb-3'>{shelter_name}</h1>
-            <h2 className="text-sm mb-1">Estamos en {zona}</h2>
-            <h2 className="text-sm mb-1">Ubicados en {location}</h2>
-            <h2 className="text-sm mt-5">Contacto: {name} - {phone}</h2>
-            <h3 className="font-bold text-sm mt-5">Descripción:</h3>
-            <p className="font-light">{description}</p>
-          </div>
-        </div>
-
+        
+      <div className="flex flex-col md:flex-row">
+      <div className="md:w-1/2">
+        <Image
+          src={imgUrl ?? ''}
+          alt={name ?? ''}
+          width={500}
+          height={300}
+          className="object-cover h-full w-full"
+        />
+      </div>
 
       <div className="md:w-1/2 p-5 flex flex-col items-center justify-center text-center relative">
         <div className="absolute top-2 right-2">
@@ -136,27 +102,9 @@ export const RefugioDetail: React.FC<IRefugios> = ({ id, name, description, imgU
         <p className="font-light">{description}</p>
       </div>
     </div>
-  
+  </div>
 
       <div className="donation-section max-w-3xl mx-auto mt-5 p-5 bg-white shadow-xl rounded-lg">
-
-        <h2 className="text-lg font-bold mb-3 text-center">Donar al refugio y deja tu huella</h2>
-        <div className="donation-amounts flex justify-around mt-3">
-          {[500, 1000, 5000].map(amount => (
-            <div
-              key={amount}
-              onClick={() => handleSelectAmount(amount)}
-              className={`cursor-pointer border-2 border-blue-500 rounded-lg p-4 text-center bg-blue-100 transition-transform transform active:bg-blue-200 active:scale-95 ${selectedAmount === amount && 'bg-blue-300'} w-32 h-48 flex flex-col justify-center items-center`}
-            >
-              <p className="text-lg font-bold">Donar</p>
-              <p className="text-2xl font-bold">${amount}</p>
-              <p className="text-xs font-bold">ARG</p>
-              <hr className="my-2" />
-              <FaPaw className="text-primary mx-auto" size={24} />
-            </div>
-          ))}
-        </div>
-
   <h2 className="text-lg font-bold mb-3 text-center">Doná al refugio y deja tu huella</h2>
   <div className="donation-amounts flex justify-around mt-3">
     <div
@@ -190,7 +138,6 @@ export const RefugioDetail: React.FC<IRefugios> = ({ id, name, description, imgU
       <FaPaw className="text-primary mx-auto" size={24} />
     </div>
   </div>
-
         <button onClick={handleAddToCart} className="mt-3 bg-tertiary hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-2xl w-full">
           Agregar donación 
         </button>
